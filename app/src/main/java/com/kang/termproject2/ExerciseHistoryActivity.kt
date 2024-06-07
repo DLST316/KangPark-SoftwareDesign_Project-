75160b1bb689d55a4afd2dca2ddb017982eb04c8
DLST316 <lolvslol66@gmail.com>
2024-06-07 16:11:19 +0900

    모든 기능 구현 완료

--- app/src/main/java/com/kang/termproject2/ExerciseHistoryActivity.kt
+++ app/src/main/java/com/kang/termproject2/ExerciseHistoryActivity.kt
@@ -26,7 +26,12 @@ class ExerciseHistoryActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.historyRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-
+        findViewById<Button>(R.id.backToMainButton).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
 
         loadExerciseHistory()
     }
